9cdc8f5696666f4f2ecbdc78afb756850eaee848
ErmolaevSD <weekendarti@yandex.ru>
2026-02-15 15:57:23 +0300

    Install Commit

---
+++ src/main/java/ru/project/analiticapp/Launcher.java
@@ -0,0 +1,9 @@
+package ru.project.analiticapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
